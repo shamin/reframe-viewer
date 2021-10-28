da2cb390a7bfaed9d3a0d626d5d1b8e8ebe90c94
shamin.meerankutty <shamin616@gmail.com>
2021-10-28 22:39:31 +0530

    Initialized a new lein project

---
+++ plugin/project.clj
@@ -0,0 +1,5 @@
+(defproject reframe-viewer "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/shamin/reframe-viewer"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns reframe-viewer.core})
---
+++ plugin/src/reframe_viewer/core.clj
@@ -0,0 +1,6 @@
+(ns reframe-viewer.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
