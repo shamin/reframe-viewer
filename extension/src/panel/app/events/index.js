cedc7d6aa666c0d61055c868e049643d51dfb5fd
shamin.meerankutty <shamin616@gmail.com>
2021-10-28 13:12:51 +0530

    Filter events option

--- extension/src/panel/app/events/index.js
+++ extension/src/panel/app/events/index.js
@@ -7,8 +7,9 @@ import { sendMessageToPage } from "../provider/channel";
 import { useApp } from "../provider";
 import { Table } from "./table";
 import { CancelIcon } from "../../../shared/icons/clear";
+import { FilterIcon } from "../../../shared/icons/filter";
 
-const headerStyles = css`
+const rowStyles = css`
   display: flex;
   background: ${COLORS.grey700};
   padding: 4px;
@@ -34,12 +35,27 @@ const cancelButtonStyles = css`
   }
 `;
 
+const getEventsToShow = (events, filterEvent) => {
+  if (filterEvent.length === 0) {
+    return [...events].sort(() => -1);
+  } else {
+    return [...events].filter((e) => e.event.includes(filterEvent));
+  }
+};
+
 export const Events = () => {
   const [event, setEvent] = useState("");
+  const [filterView, setFilterView] = useState(false);
+  const [filterEvent, setFilterEvent] = useState("");
   const { events, clearEvents } = useApp();
+
+  const toggleFilter = () => {
+    setFilterView(!filterView);
+  };
+
   return (
     <div>
-      <div css={headerStyles}>
+      <div css={rowStyles}>
         <Input
           type="text"
           placeholder="Dispatch Event eg: [:event {:data data}]"
@@ -60,8 +76,31 @@ export const Events = () => {
         >
           <CancelIcon color={COLORS.grey100} size={16} />
         </button>
+        <button
+          css={cancelButtonStyles}
+          onClick={() => {
+            toggleFilter();
+          }}
+        >
+          <FilterIcon
+            color={filterView ? COLORS.activeRed : COLORS.grey100}
+            size={14}
+          />
+        </button>
       </div>
-      <Table events={[...events].sort(() => -1)} />
+      {filterView && (
+        <div css={rowStyles}>
+          <Input
+            type="text"
+            placeholder="Filter"
+            value={filterEvent}
+            onChange={(e) => {
+              setFilterEvent(e.target.value);
+            }}
+          />
+        </div>
+      )}
+      <Table events={getEventsToShow(events, filterEvent)} />
     </div>
   );
 };
