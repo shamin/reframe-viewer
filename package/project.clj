31cd578da2ac5440f71f8166e480a816070d9882
shamin.meerankutty <shamin616@gmail.com>
2021-10-28 22:57:09 +0530

    Adding cljs package listeners, channel, etc

---
+++ package/project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojars.shamin616/reframe-viewer "0.1.1-SNAPSHOT"
+  :description "Reframe viewer extension package"
+  :url "https://github.com/shamin/reframe-viewer"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.879"]
+                 [re-frame "1.2.0"]]
+  :repl-options {:init-ns reframe-viewer.main})
--- plugin/src/reframe_viewer/core.clj
+++
@@ -1,6 +0,0 @@
-(ns reframe-viewer.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
